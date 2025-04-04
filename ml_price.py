6267db666e8bc747f7fec776b3530b0c1849cbe8
Sean Merkle <smeerkle19@gmail.com>
2025-04-04 14:27:16 -0400

    copy price file

--- ml_price.py
+++ ml_price.py
@@ -8,16 +8,16 @@ from sklearn.pipeline import Pipeline
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-def predict_units(X_units, y_units):
-    X1_train, X1_test, y1_train, y1_test = train_test_split(X_units, y_units, test_size=0.2, random_state=42)
+def predict_price(X_price, y_price):
+    X2_train, X2_test, y2_train, y2_test = train_test_split(X_price, y_price, test_size=0.2, random_state=42)
 
-    X1_train_proc = preprocessor.fit_transform(X1_train)
-    X1_test_proc = preprocessor.transform(X1_test)
+    X2_train_proc = preprocessor.fit_transform(X2_train)
+    X2_test_proc = preprocessor.transform(X2_test)
 
-    model_units = Sequential([
-        Dense(64, activation='relu', input_shape=(X1_train_proc.shape[1],)),
+    model_price = Sequential([
+        Dense(64, activation='relu', input_shape=(X2_train_proc.shape[1],)),
         Dense(32, activation='relu'),
         Dense(1)
     ])
-    model_units.compile(optimizer='adam', loss='mse', metrics=['mae'])
-    model_units.fit(X1_train_proc, y1_train, epochs=50, batch_size=32, validation_split=0.2)
+    model_price.compile(optimizer='adam', loss='mse', metrics=['mae'])
+    model_price.fit(X2_train_proc, y2_train, epochs=50, batch_size=32, validation_split=0.2)
